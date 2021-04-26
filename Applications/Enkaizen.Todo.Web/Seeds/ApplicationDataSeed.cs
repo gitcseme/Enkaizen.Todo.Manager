5cc4b9ce5886cb66a18fed75a42129a30e0e6df5
kashuvo <shuvo_CSE23@yahoo.com>
2021-04-26 13:01:06 +0600

    automatic is working with admin & users seeding

---
+++ Applications/Enkaizen.Todo.Web/Seeds/ApplicationDataSeed.cs
@@ -0,0 +1,69 @@
+﻿using Enkaizen.Todo.Dal;
+using Enkaizen.Todo.Web.Data;
+using Enkaizen.Todo.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enkaizen.Todo.Web.Seeds
+{
+    public class ApplicationDataSeed : DataSeed
+    {
+        public ApplicationDataSeed(
+            ApplicationDbContext dbContext, 
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+            : base(dbContext)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+
+            adminUser = new ApplicationUser { Email = "admin@gmail.com", UserName = "shuvo" };
+
+            regularUser1 = new ApplicationUser { Email = "regular1@gmail.com", UserName = "arif" };
+            regularUser2 = new ApplicationUser { Email = "regular2@gmail.com", UserName = "mahin" };
+
+            adminRole = new IdentityRole("Admin");
+            regularRole = new IdentityRole("Regular");
+        }
+
+        public UserManager<ApplicationUser> _userManager { get; }
+        public RoleManager<IdentityRole> _roleManager { get; }
+
+        private ApplicationUser adminUser, regularUser1, regularUser2;
+        private IdentityRole adminRole, regularRole;
+
+        public async override Task SeedAsync()
+        {
+            try
+            {
+                var result = await _userManager.CreateAsync(adminUser, "Admin$2021");
+                if (result.Succeeded)
+                {
+                    await _roleManager.CreateAsync(adminRole);
+                    await _userManager.AddToRoleAsync(adminUser, adminRole.Name);
+                }
+
+                result = await _userManager.CreateAsync(regularUser1, "Shuvo$2021");
+                if (result.Succeeded)
+                {
+                    await _roleManager.CreateAsync(regularRole);
+                    await _userManager.AddToRoleAsync(regularUser1, regularRole.Name);
+                }
+
+                result = await _userManager.CreateAsync(regularUser2, "Shuvo$2021");
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(regularUser1, regularRole.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                // EXPECTED Exception no need to catch or worry.
+            }
+        }
+    }
+}
--- Applications/Enkaizen.Todo.Web/Startup.cs
+++ Applications/Enkaizen.Todo.Web/Startup.cs
@@ -5,6 +5,7 @@ using Enkaizen.Todo.Data.Contexts;
 using Enkaizen.Todo.Data.Seeds;
 using Enkaizen.Todo.Web.Data;
 using Enkaizen.Todo.Web.Models;
+using Enkaizen.Todo.Web.Seeds;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -43,8 +44,17 @@ namespace Enkaizen.Todo.Web
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            //services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services
+                .AddIdentity<ApplicationUser, IdentityRole>()
+                .AddUserManager<UserManager<ApplicationUser>>()
+                .AddRoleManager<RoleManager<IdentityRole>>()
+                .AddSignInManager<SignInManager<ApplicationUser>>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
 
             services.AddDbContext<TodoContext>(options =>
                 options.UseSqlServer(connectionString, b => b.MigrationsAssembly(migrationAssemblyName))
@@ -56,11 +66,16 @@ namespace Enkaizen.Todo.Web
 
         public void ConfigureContainer(ContainerBuilder builder)
         {
+            builder.RegisterType<ApplicationDataSeed>().SingleInstance();
             builder.RegisterModule(new TodoDataModule(connectionString, migrationAssemblyName));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, TodoDataSeed todoDataSeed)
+        public void Configure(
+            IApplicationBuilder app, 
+            IWebHostEnvironment env, 
+            TodoDataSeed todoDataSeed, 
+            ApplicationDataSeed applicationDataSeed)
         {
             AutofacContainer = app.ApplicationServices.GetAutofacRoot();
 
@@ -92,6 +107,9 @@ namespace Enkaizen.Todo.Web
             });
 
             // Migration automation and seeding data
+            applicationDataSeed.MigrateAsync().Wait();
+            applicationDataSeed.SeedAsync().Wait();
+
             todoDataSeed.MigrateAsync().Wait();
         }
     }
